c2b9172cb924ce00c5fb73a7b2c870c9ab5e17ef
jianghong.li <ljc555@gmail.com>
2019-01-02 21:50:59 +0800

    配置框架

--- bak/sell_server/src/main/java/com/imooc/handler/SellExceptionHandler.java
+++ bak/sell_server/src/main/java/com/imooc/handler/SellExceptionHandler.java
@@ -1,17 +1,10 @@
 package com.imooc.handler;
 
-import com.imooc.VO.ResultVO;
 import com.imooc.config.ProjectUrlConfig;
-import com.imooc.exception.ResponseBankException;
-import com.imooc.exception.SellException;
 import com.imooc.exception.SellerAuthorizeException;
-import com.imooc.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -34,17 +27,5 @@ public class SellExceptionHandler {
         .concat("?returnUrl=")
         .concat(projectUrlConfig.getSell())
         .concat("/sell/seller/login"));
-    }
-
-    @ExceptionHandler(value = SellException.class)
-    @ResponseBody
-    public ResultVO handlerSellerException(SellException e) {
-        return ResultVOUtil.error(e.getCode(),e.getMessage());
-    }
-
-    @ExceptionHandler(value = ResponseBankException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public void handleResponseBankException() {
-
     }
 }
